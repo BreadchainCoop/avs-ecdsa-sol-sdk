2cfcd50d5f59b2b6faf377a93aafc7eccbb72c6d
Hunter King <hking6776@gmail.com>
2024-12-26 20:44:31 -0500

    refactor: add example-contracts folder

--- src/example-contracts/LayerConsumer.sol
+++ src/example-contracts/LayerConsumer.sol
@@ -2,13 +2,16 @@
 pragma solidity 0.8.23;
 
 import {LayerSDK} from 'contracts/LayerSDK.sol';
-import {ILayerConsumer} from 'interfaces/ILayerConsumer.sol';
 
-contract LayerConsumer is LayerSDK, ILayerConsumer {
+contract LayerConsumer is LayerSDK {
   /// @notice Initializer
   constructor(address _stakeRegistry) LayerSDK(_stakeRegistry) {}
 
-  /// @inheritdoc ILayerConsumer
+  /**
+   * @notice Validates a layer task from off-chain AVS operator
+   * @param _offchainData The off-chain data to verify
+   * @return _isValid Whether the task is valid
+   */
   function validateLayerTask(string calldata _offchainData) external view returns (bool _isValid) {
     Task memory _task = Task({dataHash: bytes32(bytes(_offchainData[0:32])), signatureData: bytes(_offchainData[32:])});
     _isValid = _validateLayerTask(_task);
